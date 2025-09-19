c7403ba6761cd9b9fdb48f5e21f0140ed3f0e93c
alaleh kaghazchi <alalehkaghazchi@gmail.com>
2025-09-19 18:10:09 +0330

    Change: ai Model changed to match the response

--- ai-description-service/src/models/aiModel.ts
+++ ai-description-service/src/models/aiModel.ts
@@ -1,9 +1,28 @@
 import { Schema, model } from "mongoose";
 
 const DescriptionSchema = new Schema({
-  tone: String,
-  palette: Object,
-  response: Object,
+  season: { type: String, required: true },
+  palettes: {
+    clothing: [String],
+    eye_makeup: [String],
+    makeup: {
+      blush: [String],
+      contour: [String],
+      highlighter: [String],
+    },
+    lipstick: [String],
+    jewelry: [String],
+  },
+  response: {
+    tone_description: String,
+    palette_type: { type: String, enum: ["گرم", "سرد", "خنثی"] },
+    clothing: String,
+    eye_makeup: String,
+    makeup: String,
+    lipstick: String,
+    jewelry: String,
+    season: String,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
